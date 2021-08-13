3ee71cd923e2da9f4ecf8da12e38304ea4afd676
kapisan <60970645+kapisan@users.noreply.github.com>
2021-08-13 22:10:08 +0900

    基本機能を追加

--- BodyWeightRecordSample/ViewController.swift
+++ BodyWeightRecordSample/ViewController.swift
@@ -7,13 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+
+    var todoItem = [String]()
+
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var textField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.textField.delegate = self
+
+        tableView.reloadData()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        tableView.reloadData()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoItem.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = todoItem[indexPath.row]
+        return cell
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    @IBAction func addActionButton(_ sender: UIButton) {
+        guard let textFieldValue = textField.text else {
+            return
+        }
+
+        todoItem.append(textFieldValue)
+        textField.text = ""
+        tableView.reloadData()
+    }
 
 }
 
