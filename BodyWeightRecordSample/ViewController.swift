56a16f534de7e0b6200c7725333f21d88469ecd3
kapisan <60970645+kapisan@users.noreply.github.com>
2021-08-14 09:55:26 +0900

    todo、2列表示、右列側完成

--- BodyWeightRecordSample/ViewController.swift
+++ BodyWeightRecordSample/ViewController.swift
@@ -7,12 +7,25 @@
 
 import UIKit
 
+struct Item {
+    let id: Int
+    let title: String
+
+    init(id: Int, title: String) {
+        self.id = id
+        self.title = title
+    }
+}
+
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
 
-    var todoItem = [String]()
+    var todoItem = [Item]()
 
     @IBOutlet private weak var tableView: UITableView!
     @IBOutlet private weak var textField: UITextField!
+
+//    @IBOutlet private weak var label1: UILabel!
+//    @IBOutlet private weak var label2: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +48,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = todoItem[indexPath.row]
+
+//        label1.text = todoItem[0][0]
+        let label1 = cell.contentView.viewWithTag(1)as! UILabel
+        let label2 = cell.contentView.viewWithTag(2)as! UILabel
+
+//        label1.text = String(todoItem[[0][0]].id)
+//        label2.text = String(todoItem[[0][0]].title)
+
+        label1.text = String(todoItem[[indexPath.row][0]].id)
+        label2.text = String(todoItem[[indexPath.row][0]].title)
         return cell
     }
 
@@ -49,7 +71,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             return
         }
 
-        todoItem.append(textFieldValue)
+        let textFieldItem: Item = Item(id: 1, title: textFieldValue)
+
+//        todoItem.append(textFieldValue)
+        todoItem.append(textFieldItem)
         textField.text = ""
         tableView.reloadData()
     }
